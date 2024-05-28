244e7c9d2a099bf423641a260376344b394a0f69
Ananbaludsamy <71933824+Ananbaludsamy@users.noreply.github.com>
2024-05-28 21:36:46 +0700

    s

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick1 = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Novice Anan baludsamy</Text>
+      </View>
+      <Text style={styles.heading}>counter Value : {counter}</Text>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button} onPress={handleClick1}>
+          <Text>Increment</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightblue",
+  },
+  heading: {
+    color: "green",
+    fontSize: 36,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  button: {
+    fontSize: 28,
+    padding: 13,
+    margin: 20,
+    width: 130,
+    alignItems: "center",
+    borderRadius: 8,
+    backgroundColor: "red",
+    elevation: 20,
+  },
+  setInitialCountButton: {
+    padding: 10,
+    fontSize: 28,
+    margin: 15,
+    borderRadius: 8,
+    backgroundColor: "pink",
+    elevation: 20,
   },
 });
+
+export default App;
